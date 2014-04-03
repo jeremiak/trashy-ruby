86f0ff194c41719b4d0745f1b979767c0dc62cb0
Jeremia Kimelman <jbkimelman@gmail.com>
2014-04-02 21:15:59 -0700

    initial up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trashy::Application.config.secret_key_base = 'ed5c534be5e090411cc3899fcbf21a1784cfbf29ea2626d2f51ca2699da2b118df7b1460d2d1e110119794bf6894c2f6ca541a5d64890ca71a5fe6c64ae4f1d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trashy::Application.config.session_store :cookie_store, key: '_trashy_session'
